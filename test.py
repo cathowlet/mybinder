405b20628d56cb5e0720e16eca4a528027eb68cf
cathowlet-mybinder-gt9d0ve0 <cathowlet-mybinder-gt9d0ve0@local>
2018-07-08 02:20:52 +0000

    Sun Jul  8 02:20:52 UTC 2018

---
+++ test.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import pexpect
+
+branch = 'test2'
+password = 'moments9021'
+
+def main(branch, password):
+    try:
+        os.system('git init')
+        os.system('git add .')
+        os.system('git commit -m "$(date)"')
+        child = pexpect.spawn('git push origin master:%s' % branch)
+        child.logfile = sys.stdout
+        child.expect('.*name.*')
+        child.sendline('1230221121@stu.cjlu.edu.cn')
+        child.expect('.*word.*')
+        child.sendline('%s' % password)
+    except Exception as e:
+        print(e)
+    
+if __name__ == "__main__":
+    main(branch, password)
