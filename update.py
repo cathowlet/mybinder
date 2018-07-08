c60c098f6c6bd64761e108124adfc4a2ee41a161
cathowlet-mybinder-gt9d0ve0 <cathowlet-mybinder-gt9d0ve0@local>
2018-07-08 02:38:23 +0000

    Sun Jul  8 02:38:23 UTC 2018

--- update.py
+++ update.py
@@ -10,6 +10,7 @@ def main(branch, password):
         os.system('git init')
         os.system('git add .')
         os.system('git commit -m "$(date)"')
+        os.system('git pull origin master:master')
         child = pexpect.spawn('git push origin master:%s' % branch)
         child.expect('.*name.*')
         child.sendline('1230221121@stu.cjlu.edu.cn')
