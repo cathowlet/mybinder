a2faea81b388ade72e718884d96bd9ed311fd88e
cathowlet <33780750+cathowlet@users.noreply.github.com>
2018-10-06 15:36:37 +0800

    Update update.py

--- update.py
+++ update.py
@@ -1,6 +1,8 @@
 #!/srv/conda/bin/python
 import pexpect
+import os
 
+os.system('git add . && git commit -m "0"')
 child = pexpect.spawnu('git push origin master')
 child.expect('.*Username.*')
 child.sendline('cathowlet1@163.com')
