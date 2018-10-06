613ff7cf590bb0bb54008bd1dd913be3c58bf69b
cathowlet <33780750+cathowlet@users.noreply.github.com>
2018-10-06 15:57:53 +0800

    Update and rename update.py to ud.py

--- ud.py
+++ ud.py
@@ -2,7 +2,7 @@
 import pexpect
 import os
 
-os.system('git add . && git commit -m "0"')
+os.system('git config --global user.email "you@example.com" && git config --global user.name "Your Name" && git add . && git commit -m "0"')
 child = pexpect.spawnu('git push origin master')
 child.expect('.*Username.*')
 child.sendline('cathowlet1@163.com')
